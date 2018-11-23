e69f180960d91fa8464062856b261e2aaf05b3f1
Russell Gordon <rgordon@lcs.on.ca>
2018-11-23 08:22:52 -0500

    Add index out of range fix so that testing in playground can occur

--- Sources/TortoiseCharmer.swift
+++ Sources/TortoiseCharmer.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 class TortoiseCharmer {
-
+    
     private(set) var tortoises: [Tortoise] = []
     private(set) var commandHistories: [(tortoiseTag: Int, commandIndex: Int)] = []
-
+    
     var commandedHandler: ((TortoiseCharmer) -> Void)?
-
+    
     init(tortoiseCount: Int = 1) {
         initialize(tortoiseCount: tortoiseCount)
     }
-
+    
     func initialize(tortoiseCount: Int = 1) {
         assert(tortoiseCount > 0)
         tortoises = []
@@ -26,35 +26,48 @@ class TortoiseCharmer {
             tortoises.append(tortoise)
         }
     }
-
+    
     @discardableResult
     func charm(with context: GraphicsContext, toFrame index: Int?) -> Int {
         let endIndex = commandHistories.count - 1
         let toIndex = min(max((index ?? endIndex), 0), endIndex)
-
+        
         context.setup()
-
+        
         var states = [State](repeating: State(), count: tortoises.count)
         for index in 0..<tortoises.count {
             states[index].canvasSize = context.size
         }
-
-        for (index, history) in commandHistories.enumerated() where index <= toIndex {
-            states[history.tortoiseTag] =
-                tortoises[history.tortoiseTag]
-                    .commands[history.commandIndex]
-                    .exexute(in: states[history.tortoiseTag], with: context.cgContext)
+        
+        // Make a copy of the array of commands for each Tortoise
+        var commandsToRun = [ Int: [Command] ]()
+        for (index, tortoise) in tortoises.enumerated() {
+            commandsToRun[index] = tortoise.commands
         }
-
+        
+        // Now iterate over all the commands in the command history and run
+        // using the copy of commands for each tortoise (saved a moment ago)
+        for (index, history) in commandHistories.enumerated() where index <= toIndex {
+            
+            // Get commands out of the dictionary for the current tortoise
+            if let commands = commandsToRun[history.tortoiseTag] {
+                
+                // Actually run the command at the given index
+                states[history.tortoiseTag] = commands[history.commandIndex]
+                    .exexute(in: states[history.tortoiseTag], with: context.cgContext)
+                
+            }
+        }
+        
         for index in 0..<tortoises.count {
             let state = states[index]
             if state.isVisible {
                 tortoises[index].drawTortoise(context.cgContext, state: state)
             }
         }
-
+        
         context.tearDown()
         return min(toIndex + 1, endIndex)
     }
-
+    
 }
